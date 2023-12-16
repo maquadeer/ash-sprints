cba24e1f66d58e5ca903b7d094280ed571751017
MOHAMMAD ABDUL QUADEER <136174168+maquadeer@users.noreply.github.com>
2023-12-16 19:48:55 +0530

    k

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"CBCcEQkPT5KQAz6ieqtHaTzocA49thuVR40VSv4X/Jg=\"\n}"
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,7 +4,7 @@ import { PostMetadata } from "./PostMetadata";
 const postPreview = (props: PostMetadata) => {
     return(
 
-     <div className="border border-violet-400 p-4 rounded-md shadow-md bg-gradient-to-r from-rose-100 to-teal-100">
+     <div className="border border-violet-400 p-4 rounded-md shadow-md h-full w-full bg-purple-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-60 border border-gray-100">
         <Link href={`/posts/${props.slug}`}>
             <h2 className="font-bold text-violet-600 hover:underline"> {props.slug} </h2>
         </Link>
